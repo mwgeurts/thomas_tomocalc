936f7e2f607438d77c6a79f4b5a1af62e711ca2f
Mark Geurts <mark.w.geurts@gmail.com>
2017-04-24 11:58:36 -0500

    removed MVCT flag, corrected sub2ind inputs

--- radpath.m
+++ radpath.m
@@ -1,4 +1,4 @@
-function  rpath=radpath(ctimages,xf,xp,zf,zp,MVCT)
+function  rpath=radpath(ctimages,xf,xp,zf,zp)
 global dose_dimensiony
 %xf,zf single focus value (outside array)
 %xp,zp multiple calc points in array
@@ -12,9 +12,6 @@ deltaz=dz/steps;
 x=xp;
 z=zp;
 rpath=0;
-ctdim=max(size(ctimages));
-ctsize=ctdim*ones(1,2);
-%hutemp=zeros(3,15,225);
 for i=1:steps
     iz=floor(z);
     iz(iz<1)=1; iz(iz>size(ctimages,3))=1;
@@ -22,12 +19,9 @@ for i=1:steps
     ix(ix<1)=1; ix(ix>size(ctimages,2))=1; %use 1 as flag both sides
     if max(iz)==1, break, end
     if max(ix)==1, break, end  
-    rhoe=ctimages(:,sub2ind([size(ctimages,2) size(ctimages,3)],ix,iz)); % electron_density() function was removed as the CT was already converted to density
+    rhoe=ctimages(:,sub2ind([size(ctimages,3) size(ctimages,2)],iz,ix)); % electron_density() function was removed as the CT was already converted to density
     rpath=rpath+rhoe;  %will multiply by dr later
 
-    %hutemp(1,:,i)=hu(:,113);
-    %hutemp(2,:,i)=rhoe(:,113);
-    %hutemp(3,:,i)=rpath(:,113);
     x=x+deltax;
     z=z+deltaz;
 end
